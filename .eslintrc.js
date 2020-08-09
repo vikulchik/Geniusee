3582e0f2b87e2d763429cfe24a04d976f924fec0
vikulchik <kvvendetta@gmail.com>
2020-08-09 21:58:46 +0300

    Add pug for html.

---
+++ .eslintrc.js
@@ -0,0 +1,14 @@
+module.exports = {
+    "env": {
+        "browser": true,
+        "es2020": true
+    },
+    "extends": "eslint:recommended",
+    "parserOptions": {
+        "ecmaVersion": 11
+    },
+    "rules": {
+        "semi": ["error", "always"],
+        "quotes": ["error", "double"]
+    }
+};
