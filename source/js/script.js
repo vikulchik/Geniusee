3da13b99b094c78c9ffc926e80afc46a6cd25186
vikulchik <kvvendetta@gmail.com>
2020-08-09 22:21:25 +0300

    Add eslint for js.

--- source/js/script.js
+++ source/js/script.js
@@ -4,40 +4,40 @@ const popupCart = document.querySelector('.popup-cart');
 const formCart = document.querySelector('.form-cart');
 const escKeyCode = 27;
 
-cart.addEventListener('click', e => {
-    e.preventDefault();
-    popupCart.classList.add('show');
-    overlay.classList.add('show');
+cart.addEventListener('click', (e) => {
+  e.preventDefault();
+  popupCart.classList.add('show');
+  overlay.classList.add('show');
 });
 
-formCart.addEventListener('submit', e => {
-    e.preventDefault();
+formCart.addEventListener('submit', (e) => {
+  e.preventDefault();
+  popupCart.classList.remove('show');
+  overlay.classList.remove('show');
+});
+
+document.addEventListener('keydown', function(e) {
+  if (e.keyCode === escKeyCode) {
     popupCart.classList.remove('show');
     overlay.classList.remove('show');
-});
-
-document.addEventListener('keydown', function(e){
-    if(e.keyCode === escKeyCode) {
-        popupCart.classList.remove('show');
-        overlay.classList.remove('show');
-    }
+  }
 });
 
 if (overlay) {
-    overlay.addEventListener('click', function (event) {
-        event.preventDefault();
-        overlay.classList.remove('show');
-        popupCart.classList.remove('show');
-    });
+  overlay.addEventListener('click', function(event) {
+    event.preventDefault();
+    overlay.classList.remove('show');
+    popupCart.classList.remove('show');
+  });
 }
 
-new Swiper('.swiper-container', {
-    navigation: {
-        nextEl: '.swiper-button-next',
-        prevEl: '.swiper-button-prev',
-    },
-    pagination: {
-        el: '.swiper-pagination',
-        clickable: true,
-    },
+new window.Swiper('.swiper-container', {
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+  pagination: {
+    el: '.swiper-pagination',
+    clickable: true,
+  },
 });
